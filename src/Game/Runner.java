a3a35ee1ececded98bcc2a92af65ba8fa0913807
BT_1N3_25 <mahmed9010@bths.edu>
2018-10-30 11:47:04 -0400

    tr

--- src/Game/Runner.java
+++ src/Game/Runner.java
@@ -3,7 +3,7 @@ package Game;
 import People.Person;
 import Rooms.Room;
 import Rooms.WinningRoom;
-
+import Rooms.LosingRoom;
 import java.util.Scanner;
 
 public class Runner {
@@ -28,6 +28,11 @@ public class Runner {
 		int x = (int)(Math.random()*building.length);
 		int y = (int)(Math.random()*building.length);
 		building[x][y] = new WinningRoom(x, y);
+
+		//Create a random losing room
+		int a = (int)(Math.random()*building.length);
+		int b = (int)(Math.random()*building.length);
+
 		 
 		 //Setup player 1 and the input scanner
 		Person player1 = new Person("FirstName", "FamilyName", 0,0);
